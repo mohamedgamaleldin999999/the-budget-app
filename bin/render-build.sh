8011220e776d7b64f893108ea4c8789bf83dd972
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-02-25 01:42:59 +0200

    Set back

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,7 @@
+#!/usr/bin/env bash
 # exit on error
-$ErrorActionPreference = "Stop"
+set -o errexit
 
-# Install dependencies
 bundle install
-
-# Precompile assets
 bundle exec rails assets:precompile
-
-# Clean assets
 bundle exec rails assets:clean
